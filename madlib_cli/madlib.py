3d542f191f876075b73a80d4e6d28fe32612bff7
evidad <errolvidad@gmail.com>
2024-01-10 20:40:44 -0800

    wrote parse_template function and passes test

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,30 @@
+import re
+
+# welcome = """
+# Welcome to Madlibs!\n
+# Please answer the following prompts and in return you'll receive a fun madlib story!\n
+# If prompted with an adjective, input an adjective\n
+# If prompted with a first name, input a first name\n
+# """
+# print(welcome)
+
+def read_template(file):
+    with open(file, 'r') as reader:
+        content = reader.read()
+        print(content)
+        # used ChatGPT and determined I need to include return content
+        return content
+
+# Used ChatGPT to figure out I can use regex to find the {}
+# Used ChatGPT to figure out re.findall and re.submethod
+def parse_template(file):
+    # takes all instances of {} and converts to tuple
+    template = tuple(re.findall(r"\{([A-Za-z0-9 '_]+)\}", file))
+    # replaces the extracted {Adjective} and {Noun} with {}
+    stripped_template = re.sub(r"\{([A-Za-z0-9 '_]+)\}", '{}', file)
+    return stripped_template, template
+
+# print(parse_template(file))  
+
+def merge():
+    pass
