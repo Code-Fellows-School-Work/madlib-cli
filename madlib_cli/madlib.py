6c2929414ed91fbfd6e53516b9134a35d129b6a3
evidad <errolvidad@gmail.com>
2024-01-15 10:46:17 -0700

    updated readme with new pull request

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,39 +1,11 @@
 import re
 from pathlib import Path
 
-# welcome = """
-# Welcome to Madlibs!\n
-# Please answer the following prompts and in return you'll receive a fun madlib story!\n
-# """
-# # print(welcome)
-
-# file_path = Path("./assets/dark_and_stormy_night_template.txt")
-# # Uncomment for debugging to checking if the path exists
-# # if file_path.exists():
-# #     content = file_path.read_text()
-# #     print(content)
-# # else:
-# #     print("File does not exist.")
-
-# def madlib_template(file_path):
-#     """Reads and prints a txt file from the argument file_path"""
-#     with open(file_path, 'r') as reader:
-#         content = reader.read()
-#         print(content)
-
-# madlib = madlib_template(file_path)
-
-
-# # can't figure out what this error is
-# def parse_template(madlib):
-#     """Parses through txt file and removes instances of {}."""
-#     # takes all instances of {} and converts to tuple
-#     template = tuple(re.findall(r"\{([A-Za-z0-9 '_]+)\}", madlib))
-#     print(template)
-
-# parse_template(madlib)
-
-
+welcome = """
+Welcome to Madlibs!\n
+Please answer the following prompts and in return you'll receive a fun madlib story!\n
+"""
+print(welcome)
 
 # Tests
 
@@ -61,7 +33,3 @@ def merge(template, tuple):
     for words in tuple:
         template = template.replace('{}', str(words), 1)
     return template
-
-# From class review, this will also pass the third test
-def merge(stripped_template, parts):
-    return stripped_template.format(*parts)
